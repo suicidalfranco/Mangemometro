1c957200cc6bb42a8ac767a06d1fba87fa71681e
suicidalfranco <bresssanfranco@gmail.com>
2015-05-28 17:36:21 +0200

    28 maggio 2015
    timer configurato per 2Hz

--- Progetto Finale/src/main.c
+++ Progetto Finale/src/main.c
@@ -12,59 +12,95 @@
 #include "stm32f4xx.h"
 #include "stm32f4xx_conf.h"
 #include "stm32f401_discovery.h"
+
+volatile uint16_t CCR1_Val = 54618;
 			
-void TIM2_IRQHandler(void)
+static void TIM_Config(void);
+
+int main(void)
 {
-  if (TIM_GetITStatus(TIM2, TIM_IT_Update) != RESET)
-  {
-    TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
-    GPIO_ToggleBits(GPIOD, GPIO_Pin_12);  
-    }    
+	uint16_t PrescalerValue = 0;
+	TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+	TIM_OCInitTypeDef TIM_OCInitStructure;
+	
+	//TIM configuration
+	TIM_Config();
+	
+	/* Compute the prescaler value */
+	PrescalerValue = (uint16_t) ((SystemCoreClock / 2) / 109236) - 1;
+	
+	/* Time base configuration */
+	TIM_TimeBaseStructure.TIM_Period = 65535;
+	TIM_TimeBaseStructure.TIM_Prescaler = 0;
+	TIM_TimeBaseStructure.TIM_ClockDivision = 0;
+	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+
+	TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
+
+	/* Prescaler configuration */
+	TIM_PrescalerConfig(TIM3, PrescalerValue, TIM_PSCReloadMode_Immediate);
+
+	/* Output Compare Timing Mode configuration: Channel1 */
+	TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_Timing;
+	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+	TIM_OCInitStructure.TIM_Pulse = CCR1_Val;
+	TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+
+	TIM_OC1Init(TIM3, &TIM_OCInitStructure);
+
+	TIM_OC1PreloadConfig(TIM3, TIM_OCPreload_Disable);
+	
+	/* TIM Interrupts enable */
+	TIM_ITConfig(TIM3, TIM_IT_CC1, ENABLE);
+
+	/* TIM3 enable counter */
+	TIM_Cmd(TIM3, ENABLE);
+
+	while (1);
 }
 
-//-----------------------------------------------------------------------------------------------------------
-
-void INTTIM_Config(void)
+/**
+  * @brief  Configure the TIM IRQ Handler.
+  * @param  None
+  * @retval None
+  */
+static void TIM_Config(void)
 {
   NVIC_InitTypeDef NVIC_InitStructure;
-  
-  /* Enable the TIM2 gloabal Interrupt */
-  NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;
+
+  /* TIM3 clock enable */
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
+
+  /* Enable the TIM3 gloabal Interrupt */
+  NVIC_InitStructure.NVIC_IRQChannel = TIM3_IRQn;
   NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
   NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
   NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
   NVIC_Init(&NVIC_InitStructure);
 
-  /* TIM2 clock enable */ 
-  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
-    
-  /* Time base configuration */
-  TIM_TimeBaseStructure.TIM_Period = ((SystemCoreClock / 4) / 1000000) - 1;  // 1 MHz (1us)
-  TIM_TimeBaseStructure.TIM_Prescaler = 0;
-  TIM_TimeBaseStructure.TIM_ClockDivision = 0;
-  TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
-  TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
-  
-  /* TIM IT enable */
-  TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
-  
-  /* TIM2 enable counter */
-  TIM_Cmd(TIM2, ENABLE);
+  /* Initialize LED4 */
+  STM_EVAL_LEDInit(LED4);
 
+  /* Turn on LED4 */
+  STM_EVAL_LEDOn(LED4);
 }
 
-int main(void)
+/**
+  * @brief  This function handles TIM3 global interrupt request.
+  * @param  None
+  * @retval None
+  */
+void TIM3_IRQHandler(void)
 {
+  uint16_t capture = 0;
 
-    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_DOWN;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-    GPIO_Init(GPIOD, &GPIO_InitStructure);
-    GPIO_ResetBits(GPIOD, GPIO_Pin_6);  
+  if (TIM_GetITStatus(TIM3, TIM_IT_CC1) != RESET)
+  {
+    TIM_ClearITPendingBit(TIM3, TIM_IT_CC1);
 
-    INTTIM_Config();
-    while(1);
+    /* LED4 toggling with frequency = 2 Hz */
+    STM_EVAL_LEDToggle(LED4);
+	capture = TIM_GetCapture1(TIM3);
+    TIM_SetCompare1(TIM3, capture + CCR1_Val);
+  }
 }
