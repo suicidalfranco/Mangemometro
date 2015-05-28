2143b3f74ba4d7ffeeae382313ca760e423a48d4
suicidalfranco <bresssanfranco@gmail.com>
2015-05-28 16:57:37 +0200

    28 maggio 2015
    aggiunto codice toggle LED con timer

--- Progetto Finale/src/main.c
+++ Progetto Finale/src/main.c
@@ -3,18 +3,68 @@
   * @file    main.c
   * @author  Ac6
   * @version V1.0
-  * @date    01-December-2013
-  * @brief   Default main function.
+  * @date    28-May-2015
+  * @brief   Progetto Finale: Magnetometro.
   ******************************************************************************
 */
 
 
 #include "stm32f4xx.h"
+#include "stm32f4xx_conf.h"
 #include "stm32f401_discovery.h"
 			
+void TIM2_IRQHandler(void)
+{
+  if (TIM_GetITStatus(TIM2, TIM_IT_Update) != RESET)
+  {
+    TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
+    GPIO_ToggleBits(GPIOD, GPIO_Pin_12);  
+    }    
+}
+
+//-----------------------------------------------------------------------------------------------------------
+
+void INTTIM_Config(void)
+{
+  NVIC_InitTypeDef NVIC_InitStructure;
+  
+  /* Enable the TIM2 gloabal Interrupt */
+  NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;
+  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
+  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+  NVIC_Init(&NVIC_InitStructure);
+
+  /* TIM2 clock enable */ 
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+    
+  /* Time base configuration */
+  TIM_TimeBaseStructure.TIM_Period = ((SystemCoreClock / 4) / 1000000) - 1;  // 1 MHz (1us)
+  TIM_TimeBaseStructure.TIM_Prescaler = 0;
+  TIM_TimeBaseStructure.TIM_ClockDivision = 0;
+  TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+  TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
+  
+  /* TIM IT enable */
+  TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
+  
+  /* TIM2 enable counter */
+  TIM_Cmd(TIM2, ENABLE);
+
+}
 
 int main(void)
 {
 
-	for(;;);
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_DOWN;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+    GPIO_Init(GPIOD, &GPIO_InitStructure);
+    GPIO_ResetBits(GPIOD, GPIO_Pin_6);  
+
+    INTTIM_Config();
+    while(1);
 }
